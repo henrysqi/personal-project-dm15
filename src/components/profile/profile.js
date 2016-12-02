d0cbd57b101fab2adef46d662aab0d50850a343b
Henry Qi <henryqi222@gmail.com>
2016-12-01 19:14:31 -0700

    styled left panel

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -46,7 +46,28 @@ class Profile extends React.Component {
                 </div>
             </div>
 
-
+            <div id="profile-left-panel">
+              <div className="profile-left-panel-item">
+                <img src="assets\images\Circle-icons-globe.svg.png" />
+                <span>Intro</span>
+              </div>
+              <div className="profile-left-panel-item">
+                <img src="assets\images\MetroUI-Apps-Windows8-Photos-icon.png" />
+                <span>Photos</span>
+              </div>
+              <div className="profile-left-panel-item">
+                <img src="assets\images\circle-friends.png" />
+                <span>Friends</span>
+              </div>
+              <div className="profile-left-panel-item" id="profile-lang">
+                <div id="profile-left-panel-item-languages">
+                  <p>English (US) · Español · Português (Brasil) · Français (France) · Deutsch</p>
+                </div>
+                <div id="profile-left-panel-item-languages-add">
+                  <button>+</button>
+                </div>
+              </div>
+            </div>
 
 
           </div>
