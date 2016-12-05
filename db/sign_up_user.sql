9f205a37570764103a42bf2eb42d9253a9e6567f
Henry Qi <henryqi222@gmail.com>
2016-12-05 14:51:46 -0700

    set default profile pic cover

--- db/sign_up_user.sql
+++ db/sign_up_user.sql
@@ -1,3 +1,3 @@
 INSERT INTO users
-(firstname, lastname, email, password, gender, bdaymonth, bdayday, bdayyear)
-VALUES($1, $2, $3, $4, $5, $6, $7, $8);
+(firstname, lastname, email, password, gender, bdaymonth, bdayday, bdayyear, profile_pic, cover_photo)
+VALUES($1, $2, $3, $4, $5, $6, $7, $8, 'assets\images\defprofpic.jpg', 'assets\images\default.jpg');
