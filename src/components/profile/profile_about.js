5bff22694eff6a8fce9f311b1b9b0e01aaa0ea41
Henry Qi <henryqi222@gmail.com>
2016-12-04 23:03:27 -0700

    revert again

--- src/components/profile/profile_about.js
+++ src/components/profile/profile_about.js
@@ -3,15 +3,7 @@ import React from 'react';
 class ProfileAbout extends React.Component {
   render() {
     return (
-      <div>
-        <p>testing dur</p>
 
-
-
-
-
-
-      </div>
     )
   }
 }
